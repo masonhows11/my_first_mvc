f7bded61993bf38377556fc55659ecefe9d4af34
mason.hows11 <mason.hows11@gmail.com>
2025-11-09 21:53:08 +0330

    do something

--- App/Framework/Router.php
+++ App/Framework/Router.php
@@ -20,19 +20,36 @@ class Router
     // GET /user/{id}/profile → UserController@show -> saved routes
     // now what url turn to regex then compare/match to saved url
 
-    public function matchUrl($current_url)
+    public function matchUrl($current_url): false|array
     {
         $params = [];
         foreach ($this->routes as $route) {
 
+            $pattern = $this->getPatternFromUrl($current_url);
 
+            if (preg_match($pattern, $route['path'], $matches)) {
+
+                $matches = array_filter($matches, "is_string", ARRAY_FILTER_USE_KEY);
+
+                $params = array_merge($matches, $route['params']);
+            }
+            return $params;
         }
+        return false;
+
+    }
+
+    private function getPatternFromUrl($current_url): string
+    {
+
     }
 
     public function routeList(): void
     {
         print_r($this->routes);
     }
+
+
 
 
 }
