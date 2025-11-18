d206f49c2145995b0d0bbc75bf8205976c10649b
mason.hows11 <mason.hows11@gmail.com>
2025-11-18 20:48:28 +0330

    test create table

--- App/Database/query.php
+++ App/Database/query.php
@@ -1,1 +1,10 @@
 <?php
+
+
+$query = 'create table users(
+         id int primary key auto_increment,
+         username varchar(30) not null,
+         fname varchar(30) not null,
+         lname varchar(30) not null,
+         password varchar(30) not null
+        );';
